74599d76e6741f7e78254b1659a2c3ab5c6c1927
YareeYare <mehulshakya230@gmail.com>
2024-01-18 04:16:29 +0530

    create Card Slider

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -8,6 +8,7 @@ import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMovies, getGenres } from '../store'
+import Slider from '../components/Slider'
 
 const Netflix = () => {
   const navigate = useNavigate()
@@ -25,7 +26,7 @@ const Netflix = () => {
 
   useEffect(() => {
     if( genresLoaded ) dispatch( fetchMovies({ type : "all" }) )
-  },[])
+  },[genresLoaded])
 
   window.onscroll = () => {
     setIsScrolled( window.scrollY === 0 ? false : true )
@@ -59,6 +60,8 @@ const Netflix = () => {
         </div>
 
       </div>
+
+    <Slider movies={movies} />
 
     </Container>
   )
